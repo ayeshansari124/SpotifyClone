c11131844c8f7dd18c84fba7416974bc7b4c3276
Ayeshaali124 <ayeshansari124@gmail.com>
2025-08-30 12:37:16 +0530

    ReactJS+tailwindCSS setup

---
+++ spotify-clone/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">React + TailwindCSS 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
