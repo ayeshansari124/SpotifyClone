a9d147caccd8265a9cb7cc40b5c4418cb5021a8c
Ayeshaali124 <ayeshansari124@gmail.com>
2025-08-30 12:56:39 +0530

    replace navbar with reactJS

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900">
-      <h1 className="text-4xl font-bold text-white">React + TailwindCSS 🚀</h1>
+    <div className="App">
+      <Home />
     </div>
   );
 }
---
+++ spotify-clone/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div className="bg-neutral-800 min-h-screen">
+      <Navbar />
+      {/* Main content goes here */}
+    </div>
+  );
+}
+
+export default Home;
